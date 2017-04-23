c346dc50581b7ffd31c0d1075b55cabfcd8ca954
Abelardo Vieira Mota <abevieiramota@gmail.com>
2017-04-23 19:39:03 -0300

    [course] r programming - week 1

---
+++ [course] r programming/notebook/foo.R
@@ -0,0 +1,4 @@
+structure(list(id = 1:4, nome = structure(c(1L, 4L, 2L, 3L), .Label = c("Abelardo Vieira Mota", 
+"Carl Jung", "Clarice Foospector", "Juliana Vieira Mota"), class = "factor"), 
+    idade = c(28L, 29L, 65L, 54L)), .Names = c("id", "nome", 
+"idade"), class = "data.frame", row.names = c(NA, -4L))
